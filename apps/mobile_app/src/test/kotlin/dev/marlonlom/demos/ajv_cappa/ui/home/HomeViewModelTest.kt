b30d2bc6d27a1fa03440eb4e4067da0f8e491d48
Marlon López <malm.gm@gmail.com>
2023-08-03 12:31:27 -0500

    feat: updated unit tests for catalog data submodule

---
+++ apps/mobile_app/src/test/kotlin/dev/marlonlom/demos/ajv_cappa/ui/home/HomeViewModelTest.kt
@@ -0,0 +1,17 @@
+package dev.marlonlom.demos.ajv_cappa.ui.home
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class HomeViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getUiState() {
+    }
+}
