c9e3db657c0680bcbd1f76447f5af5b773701139
Marlon López <malm.gm@gmail.com>
2023-08-03 22:36:24 -0500

    feat: updated mobile app dependencies

--- apps/mobile_app/build.gradle.kts
+++ apps/mobile_app/build.gradle.kts
@@ -92,6 +92,7 @@ dependencies {
   implementation(libs.androidx.compose.material3.wsc)
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.lifecycle.runtime.compose)
   implementation(libs.androidx.navigation.compose)
   implementation(libs.androidx.navigation.runtime.ktx)
   implementation(libs.coil.compose)
