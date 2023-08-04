e53998771be57dc1b5d92aef396df4236e9f475b
Marlon López <malm.gm@gmail.com>
2023-08-03 22:42:22 -0500

    feat: updated mobile app theme colors

--- apps/mobile_app/src/main/kotlin/dev/marlonlom/demos/ajv_cappa/ui/theme/Theme.kt
+++ apps/mobile_app/src/main/kotlin/dev/marlonlom/demos/ajv_cappa/ui/theme/Theme.kt
@@ -31,14 +31,43 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val brandDarkColorScheme = darkColorScheme(
-  primary = PapayaWhip80,
+  primary = BrandColors.macaroniAndCheese,
+  onPrimary = BrandColors.maroon5,
+  primaryContainer = BrandColors.maroon7,
+  onPrimaryContainer = BrandColors.bisque,
+  secondary = BrandColors.lightPink,
+  onSecondary = BrandColors.pohutukawa,
+  secondaryContainer = BrandColors.paprika,
+  onSecondaryContainer = BrandColors.mistyRose3,
+  tertiary = BrandColors.melon,
+  onTertiary = BrandColors.faluRed,
+  tertiaryContainer = BrandColors.flameRed,
+  onTertiaryContainer = BrandColors.mistyRose2,
+  error = BrandColors.melon2,
+  errorContainer = BrandColors.sangria,
+  onError = BrandColors.maroon6,
+  onErrorContainer = BrandColors.mistyRose,
+  background = BrandColors.maroon,
+  onBackground = BrandColors.peachPuff,
+  surface = BrandColors.maroon,
+  onSurface = BrandColors.peachPuff,
+  surfaceVariant = BrandColors.paco,
+  onSurfaceVariant = BrandColors.wafer,
+  outline = BrandColors.zorba,
+  inverseOnSurface = BrandColors.maroon,
+  inverseSurface = BrandColors.peachPuff,
+  inversePrimary = BrandColors.olive,
+  surfaceTint = BrandColors.macaroniAndCheese,
+  outlineVariant = BrandColors.paco,
+  scrim = BrandColors.black,
+
+  /*primary = PapayaWhip80,
   onPrimary = Color.White,
   secondary = Melon80,
   onSecondary = Color.White,
@@ -47,25 +76,58 @@ private val brandDarkColorScheme = darkColorScheme(
   background = Bone80,
   onBackground = Color.White,
   surface = Bone80,
-  onSurface = Color.White
+  onSurface = Color.White*/
 )
 
 private val brandLightColorScheme = lightColorScheme(
-  primary = PapayaWhip40,
-  onPrimary = Color.Black,
-  primaryContainer = PapayaWhip40,
-  onPrimaryContainer = Melon10,
-  secondary = Melon40,
-  onSecondary = Color.Black,
-  secondaryContainer = Melon90,
-  onSecondaryContainer = Melon10,
-  tertiary = MonaLisa40,
-  background = Bone40,
-  onBackground = BlackRussian40,
-  surface = Bone40,
-  onSurface = BlackRussian40,
-  surfaceVariant = Bone80,
-  onSurfaceVariant = Color.White
+  primary = BrandColors.olive,
+  onPrimary = BrandColors.white,
+  primaryContainer = BrandColors.bisque,
+  onPrimaryContainer = BrandColors.maroon2,
+  secondary = BrandColors.roofTerracotta,
+  onSecondary = BrandColors.white,
+  secondaryContainer = BrandColors.mistyRose3,
+  onSecondaryContainer = BrandColors.tyrianPurple,
+  tertiary = BrandColors.mexicanRed,
+  onTertiary = BrandColors.white,
+  tertiaryContainer = BrandColors.mistyRose2,
+  onTertiaryContainer = BrandColors.tyrianPurple2,
+  error = BrandColors.fireBrick,
+  errorContainer = BrandColors.mistyRose,
+  onError = BrandColors.white,
+  onErrorContainer = BrandColors.maroon3,
+  background = BrandColors.lavenderBlush,
+  onBackground = BrandColors.maroon,
+  surface = BrandColors.lavenderBlush,
+  onSurface = BrandColors.maroon,
+  surfaceVariant = BrandColors.provincialPink,
+  onSurfaceVariant = BrandColors.paco,
+  outline = BrandColors.sandDune,
+  inverseOnSurface = BrandColors.seashell,
+  inverseSurface = BrandColors.maroon4,
+  inversePrimary = BrandColors.macaroniAndCheese,
+  surfaceTint = BrandColors.olive,
+  outlineVariant = BrandColors.wafer,
+  scrim = BrandColors.black
+
+  /*primary = PapayaWhip40,
+    onPrimary = Color.Black,
+    primaryContainer = PapayaWhip40,
+    onPrimaryContainer = Melon10,
+    secondary = Melon40,
+    onSecondary = Color.Black,
+    secondaryContainer = Melon90,
+    onSecondaryContainer = Melon10,
+    tertiary = MonaLisa40,
+    onTertiary = BlackRussian40,
+    tertiaryContainer = MonaLisa90,
+    onTertiaryContainer = MonaLisa10,
+    background = Bone40,
+    onBackground = BlackRussian40,
+    surface = Bone40,
+    onSurface = BlackRussian40,
+    surfaceVariant = Bone80,
+    onSurfaceVariant = Color.White*/
 
   /* Other default colors to override
   background = Color(0xFFFFFBFE),
@@ -100,7 +162,7 @@ fun CappaTheme(
     SideEffect {
       val window = (view.context as Activity).window
       window.statusBarColor = colorScheme.primary.toArgb()
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
     }
   }
 
