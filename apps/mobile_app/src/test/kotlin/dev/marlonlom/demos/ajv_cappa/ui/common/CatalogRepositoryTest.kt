e18529f96c836ce3c73d1c57619159ff44e75b28
Marlon López <malm.gm@gmail.com>
2023-08-13 12:51:02 -0500

    feat: added unit tests for repository; updated unit tests for viewmodel

--- apps/mobile_app/src/test/kotlin/dev/marlonlom/demos/ajv_cappa/ui/common/CatalogRepositoryTest.kt
+++ apps/mobile_app/src/test/kotlin/dev/marlonlom/demos/ajv_cappa/ui/common/CatalogRepositoryTest.kt
@@ -1,5 +1,76 @@
+/*
+ * Copyright (c) 2023 Marlonlom
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package dev.marlonlom.demos.ajv_cappa.ui.common
 
-import org.junit.Assert.*
+import dev.marlonlom.demos.ajv_cappa.local.data.ProductItem
+import dev.marlonlom.demos.ajv_cappa.remote.data.CatalogDataService
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
 
-class CatalogRepositoryTest
+internal class CatalogRepositoryTest {
+
+  private lateinit var repository: CatalogRepository
+
+  @Before
+  fun setUp() {
+    repository = CatalogRepository(
+      FakeLocalDataSource(
+        CatalogDataService()
+      )
+    )
+  }
+
+  @Test
+  fun shouldReturnNonEmptyList() = runTest {
+    val products = repository.getAllProducts().first()
+    Assert.assertTrue(products.isNotEmpty())
+  }
+
+  @Test
+  fun shouldReturnSingleProductItem() = runTest {
+    val expectedItem = ProductItem(
+      id = 15396L,
+      title = "Granizado",
+      picture = "https://juanvaldez.com/wp-content/uploads/2022/10/Granizado-juan-Valdez.jpg",
+    )
+    val foundProduct = repository.getProduct(expectedItem.id).first()
+    Assert.assertNotNull(foundProduct)
+    Assert.assertEquals(expectedItem, foundProduct)
+  }
+
+  @Test
+  fun shouldReturnSingleProductItemPunctuationsList() = runTest {
+    val productId = 15396L
+    val points = repository.getPunctuations(productId).first()
+    Assert.assertTrue(points.isNotEmpty())
+  }
+
+  @Test
+  fun shouldReturnSingleProductItemPunctuationsEmptyList() = runTest {
+    val productId = 9L
+    val points = repository.getPunctuations(productId).first()
+    Assert.assertTrue(points.isEmpty())
+  }
+}
---
+++ apps/mobile_app/src/test/kotlin/dev/marlonlom/demos/ajv_cappa/ui/common/FakeLocalDataSource.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2023 Marlonlom
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package  dev.marlonlom.demos.ajv_cappa.ui.common
+
+import dev.marlonlom.demos.ajv_cappa.local.data.LocalDataSource
+import dev.marlonlom.demos.ajv_cappa.local.data.ProductItem
+import dev.marlonlom.demos.ajv_cappa.local.data.ProductItemPoint
+import dev.marlonlom.demos.ajv_cappa.remote.data.CatalogDataService
+import dev.marlonlom.demos.ajv_cappa.remote.data.CatalogItem
+import dev.marlonlom.demos.ajv_cappa.remote.data.successOr
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+internal class FakeLocalDataSource(
+  private val remoteDataService: CatalogDataService
+) : LocalDataSource {
+
+  override fun getAllProducts(): Flow<List<ProductItem>> {
+    val listResponse = remoteDataService.fetchData().successOr(emptyList())
+    return flowOf(listResponse.map { ProductItem(id = it.id, title = it.title, picture = it.picture) })
+  }
+
+  override fun getProduct(productId: Long): Flow<ProductItem> {
+    val listResponse: ProductItem = remoteDataService.fetchData()
+      .successOr(emptyList())
+      .find { it.id == productId }
+      .let {
+        if (it != null) ProductItem(it.id, it.title, it.picture) else NONE
+      }
+    return flowOf(listResponse)
+  }
+
+  override fun getPunctuations(productId: Long): Flow<List<ProductItemPoint>> {
+    val listResponse: CatalogItem = remoteDataService.fetchData()
+      .successOr(emptyList())
+      .find { it.id == productId } ?: return flowOf(emptyList())
+
+    val units = listResponse.punctuations.mapIndexed { index, punctuation ->
+      ProductItemPoint(
+        id = index.plus(1).toLong(),
+        productId = listResponse.id,
+        label = punctuation.label,
+        points = punctuation.pointsQty.toLong()
+      )
+    }
+    return flowOf(units)
+  }
+
+  override fun insertAllProducts(vararg products: ProductItem) {
+    TODO("Not yet implemented")
+  }
+
+  override fun insertAllPunctuations(vararg punctuations: ProductItemPoint) {
+    TODO("Not yet implemented")
+  }
+
+  override fun deleteAllProducts() {
+    TODO("Not yet implemented")
+  }
+
+  override fun deleteAllPunctuations() {
+    TODO("Not yet implemented")
+  }
+
+  companion object {
+    val NONE = ProductItem(-1, "", "")
+  }
+}
