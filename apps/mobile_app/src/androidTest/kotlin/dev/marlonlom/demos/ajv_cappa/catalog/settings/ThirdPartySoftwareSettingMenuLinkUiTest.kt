16f59888fb93d2feaef4f4ef4a054fb63258d2e2
Marlon López <malm.gm@gmail.com>
2023-08-30 12:59:59 -0500

    feat: added android ui tests in mobile-app

---
+++ apps/mobile_app/src/androidTest/kotlin/dev/marlonlom/demos/ajv_cappa/catalog/settings/ThirdPartySoftwareSettingMenuLinkUiTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2023 Marlonlom
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package dev.marlonlom.demos.ajv_cappa.catalog.settings
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import dev.marlonlom.demos.ajv_cappa.ui.theme.CappaTheme
+import org.junit.Rule
+import org.junit.Test
+
+class ThirdPartySoftwareSettingMenuLinkUiTest {
+
+  @get:Rule
+  val composeTestRule = createComposeRule()
+
+  @Test
+  fun shouldClickThirdPartySoftwareSettingMenuLink() {
+    composeTestRule.setContent {
+
+      CappaTheme {
+        ThirdPartySoftwareSettingMenuLink(
+          CatalogSettingsRouteParams(
+            uiState = CatalogSetting(
+              appVersion = "1.0.0",
+              isAppInDarkTheme = false,
+              isUsingDynamicColors = true,
+              privacyPolicyUrl = "",
+              termsConditionsUrl = "",
+              personalDataTreatmentPolicyUrl = ""
+            ),
+            openLicensesSectionAction = {
+              println("[openLicensesSectionAction]")
+            },
+            openExternalUrlAction = {},
+            updateBooleanSettingAction = { a, b ->
+              println("key=$a; flag=$b")
+            },
+          )
+        )
+      }
+    }
+
+    val onNodeWithText = composeTestRule.onNodeWithText("Third party software")
+    onNodeWithText.assertExists().performClick()
+  }
+}
